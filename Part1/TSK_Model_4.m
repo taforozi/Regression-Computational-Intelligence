ef74d58eab7eb797cb2859372c7152b311094d7e
taforozi <81076999+taforozi@users.noreply.github.com>
2021-04-03 15:18:50 +0300

    Update TSK_Model_4.m

--- Part1/TSK_Model_4.m
+++ Part1/TSK_Model_4.m
@@ -1,6 +1,4 @@
 %% Fuzzy Systems - Regression Part 1
-% Aforozi Thomais
-% 9291
 % 3 Membership functions - Polynomial
 
 function [fis,MSE,RMSE,R2,NMSE,NDEI,y] = TSK_Model_4(training_data,validation_data,check_data)
